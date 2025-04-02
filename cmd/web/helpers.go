bff235f803f92b789f37beb72a8424047b9d9b81
domgjntoki <fredsonmenezessumi@gmail.com>
2025-04-01 23:18:13 -0300

    routes added, new dark theme

---
+++ cmd/web/helpers.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"runtime/debug"
+)
+
+func (app *application) serverError(w http.ResponseWriter, err error) {
+	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
+	err = app.errorLog.Output(2, trace)
+	if err != nil {
+		app.errorLog.Fatal(err)
+		return
+	}
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+func (app *application) clientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
+
+func (app *application) notFound(w http.ResponseWriter) {
+	app.clientError(w, http.StatusNotFound)
+}
